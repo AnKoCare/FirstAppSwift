37105ab8790c5f48f7e8ef2af4199ebf75bafaaa
AnKoCare <trinhquocan0888816858@gmail.com>
2024-05-03 10:56:30 +0700

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Trịnh Quốc An on 14/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeClicked(_ sender: Any) {
+        imageView.image = UIImage(named: "Duck")
+        print("testGit")
+    }
+    
+}
+
